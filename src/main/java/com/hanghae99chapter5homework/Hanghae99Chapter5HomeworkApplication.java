49161d60ba135f340022114882ef0e0eb85759b8
matthewsmkim <forme3328@naver.com>
2022-10-17 10:28:04 +0900

    Create a Spring project.

---
+++ src/main/java/com/hanghae99chapter5homework/Hanghae99Chapter5HomeworkApplication.java
@@ -0,0 +1,14 @@
+package com.hanghae99chapter5homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Chapter5HomeworkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Hanghae99Chapter5HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghae99chapter5homework/Hanghae99Chapter5HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99chapter5homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99Chapter5HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
